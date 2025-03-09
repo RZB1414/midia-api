d5839650ac25fbb63311ae463fbae88131ebe6e3
Renan Buiatti <renanbuiatti14@gmail.com>
2025-03-09 15:51:58 +0700

    changed size of upload to 1000MB

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ app.use(cors({
 }))
 
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({ limit: '1000mb' }))
+app.use(express.urlencoded({ limit: '1000mb', extended: true }))
 
 app.use((req, res, next) => {
     req.setTimeout(20000, () => { 
